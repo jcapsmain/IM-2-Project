2b2d7c52e2ada325bd91592365329d804bf80321
Micro-88 <xXgen88Xx@gmail.com>
2024-07-15 21:29:28 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -87,7 +87,6 @@
   <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">     <!-- Press Start 2p Font -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Techno+Race&display=swap">    <!-- Techno Race Font -->
   <link rel="stylesheet" href="css/navbar.css">
 </head>
 
@@ -174,7 +173,7 @@
                         <div class="container">
                             <form method="post" autocomplete="off" name="user-register">
                                 <div class="form-group">
-                                    <label class="form-label" for="name">Username</label>
+                                    <label class="form-label" for="username">Username</label>
                                     <input class="form-control" placeholder="Enter Username" type="text" id="username" name="user-register-username" required>
                                 </div>
                                 <div class="form-group">
