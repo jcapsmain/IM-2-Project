c327bed36a8fc6cc84e201d6326e4f95d2882dfa
Micro-88 <xXgen88Xx@gmail.com>
2024-07-19 21:55:18 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -99,7 +99,7 @@
 </head>
 
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary custom-navbar fixed-top h-20">
+<nav class="navbar navbar-expand-lg bg-body-tertiary custom-navbar fixed-top">
     <div class="container-fluid">
     <a class="navbar-brand" href="Homepage.php">Sensei's Kitchen for Boosting and Development</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
